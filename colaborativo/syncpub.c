567ace39956dfdb5cb0f0a57cc688ddc655bf372
BidkarSolisPerez <bidkr.solis@gmail.com>
2019-09-24 22:23:34 -0600

    Colaborativo tarea 7
    Tarea 7 distribuidos

--- colaborativo/syncpub.c
+++ colaborativo/syncpub.c
@@ -1,5 +1,5 @@
 // syncpub.c
-// Synchronized publisher 
+// Synchronized publisher
 //
 #include "zhelpers.h"
 
@@ -8,13 +8,13 @@
 
 int main (void) {
   void *context = zmq_ctx_new ();
-
+int maxNumber = 0;
   //  Socket to talk to clients
-  void *publisher = zmq_socket (context, ZMQ_PUB); 
+  void *publisher = zmq_socket (context, ZMQ_PUB);
   zmq_bind (publisher, "tcp://*:5561");
 
   //  Socket to receive signals
-  void *syncservice = zmq_socket (context, ZMQ_REP); 
+  void *syncservice = zmq_socket (context, ZMQ_REP);
   zmq_bind (syncservice, "tcp://*:5562");
 
   //  Get synchronization from subscribers
@@ -22,22 +22,30 @@ int main (void) {
   int subscribers = 0;
   while (subscribers < SUBSCRIBERS_EXPECTED) {
     //  - wait for synchronization request
-    char *string = s_recv (syncservice); 
+    char *string = s_recv (syncservice);
+    int numberReceived;
+    sscanf (string, "%d",&numberReceived);
+    printf("El numero es: %d\n",numberReceived);  
+    maxNumber = (maxNumber < numberReceived) ? numberReceived : maxNumber;
     free (string);
-    // - send synchronization reply 
-    s_send (syncservice, ""); 
+    // - send synchronization reply
+    s_send (syncservice, "Number Received");
     subscribers++;
   }
 
   //  Now broadcast exactly 1M updates followed by END
   printf ("Broadcasting messages\n");
   int update_nbr;
-  for (update_nbr = 0; update_nbr < 10; update_nbr++)
-     s_send (publisher, "Rhubarb");
+  for (update_nbr = 0; update_nbr < 1; update_nbr++){
+    char numberasstring [10];
+    sprintf (numberasstring, "%d", maxNumber);
+     s_send (publisher, numberasstring);
+  }
 
-  s_send (publisher, "END");
-  zmq_close (publisher); 
-  zmq_close (syncservice); 
-  zmq_ctx_destroy (context); 
+  s_send (publisher, "END");  
+  zmq_close (publisher);
+  zmq_close (syncservice);
+  zmq_ctx_destroy (context);
+getchar();  
   return 0;
 }
--- colaborativo/syncsub.c
+++ colaborativo/syncsub.c
@@ -7,38 +7,45 @@ int main (void) {
   void *context = zmq_ctx_new ();
 
   //  First, connect our subscriber socket
-  void *subscriber = zmq_socket (context, ZMQ_SUB); 
-  zmq_connect (subscriber, "tcp://localhost:5561"); 
+  void *subscriber = zmq_socket (context, ZMQ_SUB);
+  zmq_connect (subscriber, "tcp://localhost:5561");
   zmq_setsockopt (subscriber, ZMQ_SUBSCRIBE, "", 0);
 
   //  0MQ is so fast, we need to wait a while...
-  sleep (1);
+  s_sleep (1);
 
   //  Second, synchronize with publisher
-  void *syncclient = zmq_socket (context, ZMQ_REQ); 
+  void *syncclient = zmq_socket (context, ZMQ_REQ);
   zmq_connect (syncclient, "tcp://localhost:5562");
-
+  //  Initialize random number generator
+  srandom ((unsigned) time (NULL));
+  int randomNumber;
+  randomNumber = rand();
+  char numberasstring [10];
+    sprintf (numberasstring, "%d", randomNumber);
   //  - send a synchronization request
-  s_send (syncclient, "");
+  s_send (syncclient, numberasstring);
 
   //  - wait for synchronization reply
-  char *string = s_recv (syncclient); 
+  char *string = s_recv (syncclient);
   free (string);
 
   //  Third, get our updates and report how many we got
-  int update_nbr = 0; 
+  int update_nbr = 0;
   while (1) {
-    char *string = s_recv (subscriber); 
+    char *string = s_recv (subscriber);
     if (strcmp (string, "END") == 0) {
       free (string);
-      break; 
+      break;
     }
+printf("Numero Mayor: %s",string);
     free (string);
     update_nbr++;
   }
-  printf ("Received %d updates\n", update_nbr);
-  zmq_close (subscriber); 
-  zmq_close (syncclient); 
-  zmq_ctx_destroy (context); 
+  printf ("Received %d updates\n", update_nbr);  
+  zmq_close (subscriber);
+  zmq_close (syncclient);
+  zmq_ctx_destroy (context);
+  getchar();
   return 0;
 }

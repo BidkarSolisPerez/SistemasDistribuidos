af5221a26ecb52c3c252dbaccb67d1694ae443ec
BidkarSolisPerez <bidkr.solis@gmail.com>
2019-10-09 21:47:22 -0600

    Concat string to make statement
    Statement completed

--- basesdatos/ejercicio9_1.c
+++ basesdatos/ejercicio9_1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "zhelpers.h"
 
 #include <nxjson.h>
 
@@ -22,14 +23,46 @@ int main() {
    nx_json* prof=professors->child;
    for (prof=professors->child; prof; prof=prof->next) {
      nx_json* field=prof->child;
+     char statement[100] = "";
+     char *value;
      for (field=prof->child; field; field=field->next) {
-       if (field->type==NX_JSON_STRING)
-          printf("%s => %s\n",field->key, field->text_value);
-       else if (field->type==NX_JSON_INTEGER)
-          printf("%s => %d\n",field->key, field->int_value);
-       else if (field->type==NX_JSON_DOUBLE)
-          printf("%s => %f\n",field->key, field->dbl_value);
+       if (field->type==NX_JSON_STRING){
+         printf("Es un string\n");
+         printf("%s => %s\n",field->key, field->text_value);
+         sprintf(value, "%s", field->text_value);
+         printf("Value: %s\n",value);
+         strcat(statement,value);
+         if(field->next){
+            strcat(statement,",");
+         }
+         free (value);
+         
+       }
+       else if (field->type==NX_JSON_INTEGER){
+         printf("Es un int\n");
+         printf("%s => %d\n",field->key, field->int_value);
+         sprintf(value, "%d", field->int_value);
+         printf("Value: %s\n",value);
+         strcat(statement,value);
+         if(field->next){
+            strcat(statement,",");
+         }
+         free (value);
+       }
+       else if (field->type==NX_JSON_DOUBLE){
+         printf("Es un double\n"); 
+         printf("%s => %f\n",field->key, field->dbl_value);
+         sprintf(value, "%f", field->dbl_value);
+         printf("Value: %f\n",value);
+         strcat(statement,value);
+         if(field->next){
+            strcat(statement,",");
+         }
+         free(value);
+       }
+      
      }
+     printf("%s\n",statement);
      printf("\n");
    }
 }

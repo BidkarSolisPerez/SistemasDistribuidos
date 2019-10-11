73af0c7b2c4f05037d13f54862229f80724c80f9
BidkarSolisPerez <bidkr.solis@gmail.com>
2019-10-10 21:32:57 -0600

    Ejercicio9_1 finished
    Finish Ejercicio9_1 and starting 9_2

--- basesdatos/ejercicio9_1.c
+++ basesdatos/ejercicio9_1.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "zhelpers.h"
-
+#include <sqlite3.h>
 #include <nxjson.h>
 
-int main() {
+int main()
+{
   FILE *f = fopen("data.json", "rb");
   fseek(f, 0, SEEK_END);
   long fsize = ftell(f);
-  fseek(f, 0, SEEK_SET);  /* same as rewind(f); */
+  fseek(f, 0, SEEK_SET); /* same as rewind(f); */
 
   char *input = malloc(fsize + 1);
   fread(input, 1, fsize, f);
@@ -16,53 +17,80 @@ int main() {
 
   input[fsize] = 0;
 
-  const nx_json* json=nx_json_parse_utf8(input);
+  const nx_json *json = nx_json_parse_utf8(input);
 
-  const nx_json* professors = nx_json_get(json, "professors");
+  const nx_json *professors = nx_json_get(json, "professors");
 
-   nx_json* prof=professors->child;
-   for (prof=professors->child; prof; prof=prof->next) {
-     nx_json* field=prof->child;
-     char statement[100] = "";
-     char *value;
-     for (field=prof->child; field; field=field->next) {
-       if (field->type==NX_JSON_STRING){
-         printf("Es un string\n");
-         printf("%s => %s\n",field->key, field->text_value);
-         sprintf(value, "%s", field->text_value);
-         printf("Value: %s\n",value);
-         strcat(statement,value);
-         if(field->next){
-            strcat(statement,",");
-         }
-         free (value);
-         
-       }
-       else if (field->type==NX_JSON_INTEGER){
-         printf("Es un int\n");
-         printf("%s => %d\n",field->key, field->int_value);
-         sprintf(value, "%d", field->int_value);
-         printf("Value: %s\n",value);
-         strcat(statement,value);
-         if(field->next){
-            strcat(statement,",");
-         }
-         free (value);
-       }
-       else if (field->type==NX_JSON_DOUBLE){
-         printf("Es un double\n"); 
-         printf("%s => %f\n",field->key, field->dbl_value);
-         sprintf(value, "%f", field->dbl_value);
-         printf("Value: %f\n",value);
-         strcat(statement,value);
-         if(field->next){
-            strcat(statement,",");
-         }
-         free(value);
-       }
-      
-     }
-     printf("%s\n",statement);
-     printf("\n");
-   }
+  nx_json *prof = professors->child;
+  for (prof = professors->child; prof; prof = prof->next)
+  {
+    nx_json *field = prof->child;
+    char statement[100] = "INSERT INTO profesor VALUES(";
+    char *value;
+    for (field = prof->child; field; field = field->next)
+    {
+      if (field->type == NX_JSON_STRING)
+      {
+        printf("Es un string\n");
+        printf("%s => %s\n", field->key, field->text_value);
+        sprintf(value, "%s", field->text_value);
+        printf("Value: %s\n", value);
+        strcat(statement, "'");
+        strcat(statement, value);
+        strcat(statement, "'");
+        printf("Statement: %s\n", statement);
+        if (field->next)
+        {
+          strcat(statement, ",");
+        }
+      }
+      else if (field->type == NX_JSON_INTEGER)
+      {
+        printf("Es un int\n");
+        printf("%s => %d\n", field->key, field->int_value);
+        sprintf(value, "%d", field->int_value);
+        printf("Value: %s\n", value);
+        strcat(statement, value);
+        if (field->next)
+        {
+          strcat(statement, ",");
+        }
+      }
+      else if (field->type == NX_JSON_DOUBLE)
+      {
+        printf("Es un double\n");
+        printf("%s => %f\n", field->key, field->dbl_value);
+        sprintf(value, "%f", field->dbl_value);
+        printf("Value: %f\n", value);
+        strcat(statement, value);
+        if (field->next)
+        {
+          strcat(statement, ",");
+        }
+      }
+    }
+    strcat(statement, ");");
+    printf("%s\n", statement);
+    printf("\n");
+
+    //Database
+    sqlite3 *db;
+    char *err_msg = 0;
+
+    int rc = sqlite3_open("profesor.db", &db);
+
+    rc = sqlite3_exec(db, statement, 0, 0, &err_msg);
+
+    if (rc != SQLITE_OK)
+    {
+      fprintf(stderr, "SQL error: %s\n", err_msg);
+      sqlite3_free(err_msg);
+      sqlite3_close(db);
+      return 1;
+    }
+
+    sqlite3_close(db);
+  }
+
+  return 0;
 }
